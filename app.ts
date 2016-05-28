83067d0b677e36fb9f398c6fb3aece2211820da9
Spencer Davies <snwmn24@gmail.com>
2016-05-28 14:45:50 -0600

    got the main grid working

--- app.ts
+++ app.ts
@@ -29,4 +29,27 @@ export class Bunny extends Animal {
 var animal = new Animal('George', 4);
 animal.getName();
 
-console.log("here");
+document.body.style.backgroundColor = "black"; 
+
+for(var row = 0; row < 3; row++) {
+    
+    var containerDiv = document.createElement('div');
+    containerDiv.id = "row-" + row;
+    containerDiv.style.width = "300px";
+    containerDiv.style.height = "75px";
+    document.body.appendChild(containerDiv);
+
+    for(var col = 0; col < 3; col++) {
+        console.log(row + " " + col);
+
+        var gridDiv = document.createElement('div');
+        gridDiv.id = '' + row + ',' + col;
+        gridDiv.style.width = "50px";
+        gridDiv.style.height = "50px";
+        gridDiv.style.margin = "12.5px";
+        gridDiv.style.backgroundColor = "white";
+        gridDiv.style.cssFloat = "left";
+        document.getElementById('row-' + row).appendChild(gridDiv); 
+    }
+}
+
